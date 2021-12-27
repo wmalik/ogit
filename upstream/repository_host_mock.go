9b06aea6d32de5612f6d4633c1924494deaf9bf6
Wasif Malik <wmalik@gmail.com>
2021-12-27 13:17:54 +0100

    Add owner, description, browserurl, cloneurl to upstream repo

--- upstream/repository_host_mock.go
+++ upstream/repository_host_mock.go
@@ -5,12 +5,31 @@ import (
 )
 
 type MockRepository struct {
-	Owner string
-	Name  string
+	Owner       string
+	Name        string
+	Description string
+	BrowserURL  string
+	CloneURL    string
 }
 
 func (r *MockRepository) GetName() string {
 	return r.Name
+}
+
+func (r *MockRepository) GetOwner() string {
+	return r.Owner
+}
+
+func (r *MockRepository) GetDescription() string {
+	return r.Description
+}
+
+func (r *MockRepository) GetBrowserURL() string {
+	return r.BrowserURL
+}
+
+func (r *MockRepository) GetCloneURL() string {
+	return r.CloneURL
 }
 
 type MockClient struct {
@@ -33,7 +52,7 @@ func (c *MockClient) GetRepositories(ctx context.Context, owners []string) []Hos
 	}
 	res := []HostRepository{}
 	for i, repo := range c.repositories {
-		if _, found := inputOwners[repo.Owner]; found {
+		if _, found := inputOwners[repo.GetOwner()]; found {
 			res = append(res, &c.repositories[i])
 		}
 	}
