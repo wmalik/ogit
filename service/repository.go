2fecafe1e733711bcf4f5574065696499800051b
Wasif Malik <wmalik@gmail.com>
2021-12-29 11:01:57 +0100

    Expose owner, desc, browserurl, cloneurl in RepositoryService

--- service/repository.go
+++ service/repository.go
@@ -7,7 +7,11 @@ import (
 )
 
 type Repository struct {
-	Name string
+	Name        string
+	Owner       string
+	Description string
+	BrowserURL  string
+	CloneURL    string
 }
 
 type Repositories []Repository
@@ -28,7 +32,11 @@ func (r *RepositoryService) GetRepositoriesByOwners(ctx context.Context, owners 
 
 	res := make(Repositories, len(repositories))
 	for i, repo := range repositories {
+		res[i].Owner = repo.GetOwner()
 		res[i].Name = repo.GetName()
+		res[i].Description = repo.GetDescription()
+		res[i].BrowserURL = repo.GetBrowserURL()
+		res[i].CloneURL = repo.GetCloneURL()
 	}
 	return &res, nil
 }
