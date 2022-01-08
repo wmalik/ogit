ad2a4888467de0ab90ae3feee77fe0fb2e46fe16
Wasif Malik <wmalik@gmail.com>
2022-01-08 16:27:06 +0100

    Fix browser url tests

--- service/repository_test.go
+++ service/repository_test.go
@@ -31,25 +31,28 @@ var _ = Describe("Repository service", func() {
 		BeforeEach(func() {
 			client := upstream.NewMockClient().WithRepositories([]upstream.MockRepository{
 				{
-					Owner:       "wmalik",
-					Name:        "ogit",
-					Description: "TUI for browsing GitHub and GitLab orgnizations",
-					BrowserURL:  "https://github.com/wmalik/ogit",
-					CloneURL:    "https://github.com/wmalik/ogit.git",
+					Owner:                  "wmalik",
+					Name:                   "ogit",
+					Description:            "TUI for browsing GitHub and GitLab orgnizations",
+					BrowserHomepageURL:     "https://github.com/wmalik/ogit",
+					BrowserPullRequestsURL: "https://github.com/wmalik/ogit/pulls",
+					CloneURL:               "https://github.com/wmalik/ogit.git",
 				},
 				{
-					Owner:       "wmalik",
-					Name:        "dotfiles",
-					Description: "wmalik's config files",
-					BrowserURL:  "https://github.com/wmalik/dotfiles",
-					CloneURL:    "https://github.com/wmalik/dotfiles.git",
+					Owner:                  "wmalik",
+					Name:                   "dotfiles",
+					Description:            "wmalik's config files",
+					BrowserHomepageURL:     "https://github.com/wmalik/dotfiles",
+					BrowserPullRequestsURL: "https://github.com/wmalik/dotfiles/pulls",
+					CloneURL:               "https://github.com/wmalik/dotfiles.git",
 				},
 				{
-					Owner:       "padawin",
-					Name:        "dotfiles",
-					Description: "padawin's config files",
-					BrowserURL:  "https://github.com/padawin/dotfiles",
-					CloneURL:    "https://github.com/padawin/dotfiles.git",
+					Owner:                  "padawin",
+					Name:                   "dotfiles",
+					Description:            "padawin's config files",
+					BrowserHomepageURL:     "https://github.com/padawin/dotfiles",
+					BrowserPullRequestsURL: "https://github.com/padawin/dotfiles/pulls",
+					CloneURL:               "https://github.com/padawin/dotfiles.git",
 				},
 			})
 			repoService = service.NewRepositoryService(client)
@@ -60,11 +63,13 @@ var _ = Describe("Repository service", func() {
 			Expect(len(*repositories)).To(Equal(2))
 			Expect((*repositories)[0].Name).To(Equal("ogit"))
 			Expect((*repositories)[0].Description).To(Equal("TUI for browsing GitHub and GitLab orgnizations"))
-			Expect((*repositories)[0].BrowserURL).To(Equal("https://github.com/wmalik/ogit"))
+			Expect((*repositories)[0].BrowserHomepageURL).To(Equal("https://github.com/wmalik/ogit"))
+			Expect((*repositories)[0].BrowserPullRequestsURL).To(Equal("https://github.com/wmalik/ogit/pulls"))
 			Expect((*repositories)[0].CloneURL).To(Equal("https://github.com/wmalik/ogit.git"))
 			Expect((*repositories)[1].Name).To(Equal("dotfiles"))
 			Expect((*repositories)[1].Description).To(Equal("wmalik's config files"))
-			Expect((*repositories)[1].BrowserURL).To(Equal("https://github.com/wmalik/dotfiles"))
+			Expect((*repositories)[1].BrowserHomepageURL).To(Equal("https://github.com/wmalik/dotfiles"))
+			Expect((*repositories)[1].BrowserPullRequestsURL).To(Equal("https://github.com/wmalik/dotfiles/pulls"))
 			Expect((*repositories)[1].CloneURL).To(Equal("https://github.com/wmalik/dotfiles.git"))
 		})
 	})
