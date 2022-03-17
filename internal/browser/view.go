4377d7b630f581cb59451f595a215a4035be56fd
Wasif <wmalik@gmail.com>
2022-03-17 21:09:11 +0100

    Align function args

--- internal/browser/view.go
+++ internal/browser/view.go
@@ -5,7 +5,8 @@ import (
 )
 
 func (m Model) View() string {
-	return lipgloss.JoinVertical(lipgloss.Left,
+	return lipgloss.JoinVertical(
+		lipgloss.Left,
 		appStyle.Render(m.list.View()),
 		bottomStatusBarStyle.Render(m.bottomStatusBar),
 	)
